41b4f2e2878c402c63987e6d3c402cad197be4e4
yeric10 <73600753@continental.edu.pe>
2022-10-11 22:03:31 -0500

    Nuevo

--- EJercicios.py
+++ EJercicios.py
@@ -50,3 +50,23 @@ print("ID: " + id)
 # =============================================================================================================================
 
 
+# Crear tu propia Lista emulando Do While. Hallar la suma de todos los valores de tu lista
+lista = []
+total = 0
+
+while True:
+    valor = int(input("Ingrese valores para tu lista, presiona 0 para terminar: "))
+    if valor != 0:
+        lista.append(valor)
+        total += lista
+    else:
+        break
+
+for i in range(len(lista)):
+    total += lista[i]
+
+print("\n")
+print("La Longitud  de tu Lista: ", len(lista))
+print("Mi Lista: ", lista)
+print("La suma de todos los valores de tu Lista: ", total)
+
