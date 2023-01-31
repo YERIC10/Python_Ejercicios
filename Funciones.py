49c1a906f05233361336bbc8e5a6fc766f90451d
yeric10 <73600753@continental.edu.pe>
2023-01-31 17:09:59 -0500

    Nuevo

---
+++ Funciones.py
@@ -0,0 +1,23 @@
+# FUNCIONES BÁSICAS EN PYTHON
+
+    #¿QUÉ SON LAS FUNCIONES?
+        # Las funciones obtienen un Objeto y un Resultado, ya sea, si contienen argumentos o no
+
+    # ¿QUÉ SON LOS ARGUMENTOS?
+        # Los argumentos son cadenas o en sí números, ya sea por variables o funciones mismas que permiten brindar
+        # para obtener un resultado.
+
+# FUNCIONES
+
+    # print() = Imprime una cadena
+                        # puede contener varios argumentes o no tenerlos
+                        # puede esperar cualquier tipo de dato, ya sea, cadenas, números, objetos, valores lógicos, etc.
+
+        print("¡HOLA MUNDO!, SOY YERIC")
+        print()
+        print("SUMA",2+3+6+7+8)
+
+
+
+
+
