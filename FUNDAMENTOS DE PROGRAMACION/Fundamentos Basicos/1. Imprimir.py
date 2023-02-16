6a72228b69dc0fd2ba5a436bda29cbd119143a4f
yeric10 <73600753@continental.edu.pe>
2023-02-16 10:43:32 -0500

    ACTUALIZACIONES

---
+++ FUNDAMENTOS DE PROGRAMACION/Fundamentos Basicos/1. Imprimir.py
@@ -0,0 +1,58 @@
+
+# print = Es una funcion que permite generar una salida en consola. Invoca argumentos o ningundo
+    # Arguemntos = Cadenas, Numeros, variables, y palabras claves. etc.
+
+
+print (" HOLA MUNDO ")      #-> "HOLA MUNDO" es una cadena que se envia como argumente a la funcion 'print' y es imprimido
+                            # por la funcion 'print'en consola
+
+
+#__________________________________________________________________________________________________________________________________
+
+
+    # Ejemplos 1
+
+Variable = 12       # Esto es una variable
+print("\n1. IMPRIME ARGUMENTO")     # \n = Es un salto de Linea.
+print (Variable)    # La variable toma como argumento para la funcion 'print'. La funcion print imprime el argumento que contine un valor
+
+
+#__________________________________________________________________________________________________________________________________
+
+
+    # Ejemplo 2
+
+Variable1 = "TE QUIERO MUCHO"   # Mis Variables
+Variable2 = 2
+
+print ("\n2. CONCATENA ',' TIPOS DE VARIABLES (NUMERICO Y CADENA)")
+print (Variable2 , Variable1)   # Los argumentos (Variable1 y Variable2) se concatena  en la funcion print por medio de: ','.
+
+        # OJO = Cuando deseamos utilizar ',' solo permite concatenar variables de tipo: 'Numerico', 'Cadena'.
+
+
+#__________________________________________________________________________________________________________________________________
+
+
+    # Ejemplo 3
+
+Variable3 = "YERIC"                  # Mis Variables
+Variable4 = " SABE PROGRAMAR"
+
+print ("\n3. CONCATENA '+' VARIABLES DE TIPO 'CADENA'")
+print (Variable3 + Variable4)   # Los argumentos (Variable3 y Variable4) se concatena  en la funcion print por medio de: '+'.
+
+        # OJO = Cuando deseamos utilizar '+' en 'cadenas', concatena las variables y es mostrado en consola.
+        # Asimismo, cuando deseamos usar '+' en variables 'Numericas' esto funcionara como una operacion aritmetica.
+
+
+#__________________________________________________________________________________________________________________________________
+
+    # Ejemplo 4
+
+Variable5 = 15                      # Mis Variables
+Variable6 = 20
+
+print ("\n4. OPERACION ARITMETICA '+' CON VARIABLES NUMERICAS")
+print (Variable5 + Variable6) # Los argumentos (Variable5 y Variable6) realizan la operacion aritmetica 'Suma'
+
---
+++ FUNDAMENTOS DE PROGRAMACION/Fundamentos Basicos/1.1. Palabras Claves.py
@@ -0,0 +1,48 @@
+
+#  ARGUEMENTOS DE PALABRAS CLAVES
+
+    # Son mecanismos de pasar de un argumento a otro.
+
+    # Palabras Claves = end=" "
+    # Palabras Claves = sep=" "
+
+
+# end=" " = esta palabra clave que atrae a los argumentos de una linea superior a su linea. Es decir 2 oraciones en diferentes lineas
+                # pueden ser mostradas en una sola linea.
+
+print("\nPALABRA CLAVE =>", 'end=""')
+
+    # Ejemplo 1
+
+print ("\n\t1.", 'end=""' )
+print ("\n\tHOLA, SOY PROGRAMADOR", end="")     # Dentro de la comillas del 'end' permite ingresar una cadena y juntan con el argumento
+print (". Y TÚ?")                             # siguiente.
+
+    # Ejemplo 2
+
+print("\n\t2.", 'end=" "')
+print("\n\tMI NOMBRE ES:", end=" ")
+print("YERIC")
+
+    # Ejemplo 3
+
+print("\n\t3.", 'end="LUIS"')
+print("\n\tHOLA, SOY YERIC", end=" LUIS")
+print(" FLORES LAPA")
+
+
+# sep=" " = esta es una palabra clave que reemplaza los espacios entre argumentos por cadenas.
+
+print("\nPALABRA CLAVE =>", 'sep=""')
+
+    # EJEMPLO 1
+print("\n\t1.", 'sep=""')
+print("\n\t5","+","6","=","10", sep="")     # Dentro de las comillas del 'sep=""' puedes ingresar cadenas que reemplacen a los
+                                            # espacion vacios brindados por las comas o como tambien eliminar los espacios sin
+                                            # ingresar ninguna cada al 'sep=""'
+
+    #EJEMPLO 2
+print("\n\t2.", 'sep="+"')
+print("\n\t5", "15", sep=" + ", end=" = ")
+print("20")
+
