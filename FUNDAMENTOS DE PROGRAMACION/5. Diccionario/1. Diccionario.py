7bda6367702ee592d3cf0a064a6c130d5d718ad1
yeric10 <73600753@continental.edu.pe>
2023-10-27 12:31:53 -0500

    Diccionario

---
+++ FUNDAMENTOS DE PROGRAMACION/5. Diccionario/1. Diccionario.py
@@ -0,0 +1,27 @@
+# QUE ES UN DICCIONARIO
+# El diccionario es otro tipo de estructura de datos de Python. No es una secuencia
+# (pero puede adaptarse fácilmente a un procesamiento secuencial)
+# y además es mutable.
+
+# Un DICCIONARIO es un conjunto de pares de (claves y valores).
+
+# Cada Clave debe ser Unica
+# Una clave puede ser un tipo de dato de cualquier tipo
+# Un diccionario no es una lista, asi que no tienen un orden de almacenamiento
+# Un diccionario es una herramienta de un solo sentido, es decir: un diccionario español a francés,
+# pero no en viceversa, osea de frances a Españo.
+
+
+# EJEMPLO 1 - Crear un Diccionario
+# diccionario {clave: valores, clave: valores, ...,}
+
+dictionary = {"gato" : "chat", "perro" : "chien", "caballo" : "cheval"}
+phone_numbers = {'jefe': 5551234567, 'Suzy': 22657854310}
+empty_dictionary = {}
+
+print(dictionary)
+print(phone_numbers)
+print(empty_dictionary)
+
+
+# OBSERVACIONES: Las cadenas los podrias implemtar con: (" " o ' '), no afecta el resultado
---
+++ FUNDAMENTOS DE PROGRAMACION/5. Diccionario/2. Diccionario - Buscar Valores.py
@@ -0,0 +1,9 @@
+
+# EJEMPLO 1 - Buscar Valores
+
+diccionario = {"gato" : "chat", "perro" : "chien", "caballo" : "cheval"}
+numero_telefonico = {'yeric' : 9891231392, "luis" : 9342554543}
+diccionario_vacio = {}
+
+print(diccionario['gato'])
+print(numero_telefonico["luis"])
